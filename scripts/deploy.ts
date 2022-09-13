31a46259ee1c6e6a4b0183d6c5a70b52f153c253
Author <author@example.com>
2022-09-13 16:02:01 +0300

    Extract Master layer.
    1. Delegate specific Piggy Banks creation to Factories.
    2. Keep registry of Factories and Piggy Banks they create in Master.
    2.1. Make it possible to register new Factories in Master.
    2.2. Store details in Master of each new Piggy Bank once it's created by a Factory.

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,12 +1,35 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const PiggyBankFactory = await ethers.getContractFactory("PiggyBankFactory");
-  const piggyBankFactory = await PiggyBankFactory.deploy();
+  const PiggyBankMaster = await ethers.getContractFactory("PiggyBankMaster");
+  const piggyBankMaster = await PiggyBankMaster.deploy();
+  await piggyBankMaster.deployed();
+  console.log(`PiggyBankMaster deployed to ${piggyBankMaster.address}.`);
 
-  await piggyBankFactory.deployed();
+  const AmountPiggyBankFactory = await ethers.getContractFactory("AmountPiggyBankFactory");
+  const amountPiggyBankFactory = await AmountPiggyBankFactory.deploy(piggyBankMaster.address);
+  await amountPiggyBankFactory.deployed();
+  console.log(`AmountPiggyBankFactory deployed to ${amountPiggyBankFactory.address}.`);
 
-  console.log(`PiggyBankFactory deployed to ${piggyBankFactory.address}.`);
+  const ApprovePiggyBankFactory = await ethers.getContractFactory("ApprovePiggyBankFactory");
+  const approvePiggyBankFactory = await ApprovePiggyBankFactory.deploy(piggyBankMaster.address);
+  await approvePiggyBankFactory.deployed();
+  console.log(`ApprovePiggyBankFactory deployed to ${approvePiggyBankFactory.address}.`);
+
+  const TimePiggyBankFactory = await ethers.getContractFactory("TimePiggyBankFactory");
+  const timePiggyBankFactory = await TimePiggyBankFactory.deploy(piggyBankMaster.address);
+  await timePiggyBankFactory.deployed();
+  console.log(`TimePiggyBankFactory deployed to ${timePiggyBankFactory.address}.`);
+
+  await registerPiggyBankFactory("Amount", amountPiggyBankFactory.address);
+  await registerPiggyBankFactory("Approve", approvePiggyBankFactory.address);
+  await registerPiggyBankFactory("Time", timePiggyBankFactory.address);
+
+  async function registerPiggyBankFactory(piggyBankType: string, factoryAddress: string) {    
+    const tx = await piggyBankMaster.registerPiggyBankFactory(piggyBankType, factoryAddress);
+    await tx.wait();
+    console.log(`Factory of Piggy Banks of "${piggyBankType}" type is registered.`);
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
